9bddf19586c1d2f9bae1eafce67a94c913356e4f
titanicwork123 <44313265+titanicwork123@users.noreply.github.com>
2018-10-27 22:49:36 +0300

    Update wes.js

--- wes.js
+++ wes.js
@@ -730,39 +730,4 @@ client.on("message", msg => {
 });
 
 
-
-    var argresult = message.content.split(`).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(تم تغيير البلاينق الى   ${argresult})
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'wt')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(تَم تغيير الواتشينق الى   ${argresult})
-  } else 
-  if (message.content.startsWith(adminprefix + 'ls')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(تَم تغيير الليسينينق الى   ${argresult})
-  } else
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
-      message.channel.send(تم تغييرك حالتك بالتويتش الى   ${argresult})
-  }
-  if (message.content.startsWith(adminprefix + 'sn')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(جاري تغيير الأسم لـ ..${argresult} )
-} else
-if (message.content.startsWith(adminprefix + 'sa')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(جاري تغيير الأفتار... : `);
-}
-});
-
-
-
 client.login('NTA1ODI1MDkxNDEwNzIyODE3.DrZOHQ.C6m0ySbmKtiR4lHsi1eWJQ0dU3w');
